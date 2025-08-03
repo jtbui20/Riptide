dda8f19852fbfedbd209809bb5df771f6021e1ec
James Bui <jtbui20@gmail.com>
2025-08-03 16:05:56 +1000

    Added random spawning area & spawner wave system + other stuff

--- Assets/Scripts/riptide_game/StaticCreaturesManager.cs
+++ Assets/Scripts/riptide_game/StaticCreaturesManager.cs
@@ -1,20 +1,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class StaticCreaturesManager : MonoBehaviour
 {
     List<BasicCreatureBehaviour> allCreatures;
+    [Header("Dump Creatures here")]
+    public List<GameObject> creaturePrefabs;
     public int CreatureCount => allCreatures.Count;
     public int MaxCount = 0;
     public bool AreAllCreaturesCaptured => allCreatures.Count == 0;
 
+    public Collider spawnAreaCollider;
+
     public event System.Action<int, int> OnCreatureCaptured;
     public event System.Action OnAllCreaturesCaptured;
+
+    public int WaveIndex = 0;
+
+    public List<int> SpawnPattern = new List<int> { 1, 1, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7 };
     public void Start()
     {
+        // TryGetAllCreatures();
+        SpawnWave();
         TryGetAllCreatures();
         DisableCreatures();
+    }
+
+    public void SpawnWave()
+    {
+        List<BasicCreatureBehaviour> creaturesToSpawn = new List<BasicCreatureBehaviour>();
+        // Select number of creatures based on pattern
+        int numberToSpawn = WaveIndex < SpawnPattern.Count ? SpawnPattern[WaveIndex] : 1;
+
+        for (int i = 0; i < numberToSpawn; i++)
+        {
+            // Pick a random location within the spawn area
+            Vector3 randomPosition = new Vector3(
+                Random.Range(spawnAreaCollider.bounds.min.x, spawnAreaCollider.bounds.max.x),
+                spawnAreaCollider.bounds.center.y, // Assuming y is the height of the spawn area
+                Random.Range(spawnAreaCollider.bounds.min.z, spawnAreaCollider.bounds.max.z)
+            );
+
+            // Sample to NavMesh to ensure the position is valid
+            if (NavMesh.SamplePosition(randomPosition, out NavMeshHit hit, 1.0f, NavMesh.AllAreas))
+            {
+                // Pick a random creature prefab from the list
+                GameObject creaturePrefab = creaturePrefabs[Random.Range(0, creaturePrefabs.Count)];
+                // Create and initialize the creature
+                BasicCreatureBehaviour newCreature = Instantiate(creaturePrefab, hit.position, Quaternion.identity).GetComponent<BasicCreatureBehaviour>();
+                creaturesToSpawn.Add(newCreature);
+            }
+        }
+
+        // Add the newly spawned creatures to the allCreatures list
+        if (allCreatures == null)
+        {
+            allCreatures = new List<BasicCreatureBehaviour>();
+        }
+        allCreatures.AddRange(creaturesToSpawn);
     }
 
     public void EnableCreatures()
